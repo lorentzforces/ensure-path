83caf1bb59554d5a9cc5e8ef09b5e5b9b6762c32
Lorentz <25649139+lorentzforces@users.noreply.github.com>
2024-01-28 15:23:31 -0600

    ignore/remove whitespace-only entries in input PATH string

--- internal/path_tools/path_tools.go
+++ internal/path_tools/path_tools.go
@@ -1,6 +1,10 @@
 package path_tools
 
-import "strings"
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
 
 const delimiter string = ":"
 
@@ -30,17 +34,18 @@ func EnsureFirst(entry, path string) string {
 	return strings.Join(pathEntries.entries, delimiter)
 }
 
-const stringNotPresent int = -1
-
 type splitPath struct {
 	entries []string
 	entryIndex int
 }
 
+const stringNotPresent int = -1
+
 // Parses the provided path, splitting it into its component entries. It will record if and where
 // the incoming entry was seen. Regardless, the returned struct will be modified to add the
 // incoming entry at the beginning, and the consumer can decide whether to use this new set of
-// entries or to discard it.
+// entries or to discard it. Whitespace-only entries will be discarded (as far as I know, the only
+// reason a whitespace-only entry would appear in a PATH string is an error).
 func checkPath(entry, path string) splitPath {
 	if len(path) == 0 {
 		return splitPath{
@@ -55,7 +60,12 @@ func checkPath(entry, path string) splitPath {
 	entryIndex := int(-1)
 	modifiedEntries = append(modifiedEntries, entry)
 	for i, existing := range rawEntries {
-		if existing == entry && entryIndex == -1 {
+		whitespaceOnly, _ := regexp.MatchString("\\s", existing)
+		if (whitespaceOnly) {
+			continue
+		}
+
+		if existing == entry && entryIndex == stringNotPresent {
 			entryIndex = i
 		} else {
 			modifiedEntries = append(modifiedEntries, existing)
@@ -78,4 +88,14 @@ func (sp splitPath) entryPresent() bool {
 
 func (sp splitPath) entryFirst() bool {
 	return sp.entryIndex == 0
+}
+
+func (sp splitPath) printDebug() string {
+	var debugStr strings.Builder;
+	debugStr.WriteString("[\n")
+	for _, entry := range sp.entries {
+		debugStr.WriteString(fmt.Sprintf("  \"%s\",\n", entry))
+	}
+	debugStr.WriteString("]\n")
+	return debugStr.String()
 }
